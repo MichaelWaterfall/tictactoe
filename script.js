e9a55d5b4b973f0521a419468fb8e2d6a4f86db1
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-07-22 12:32:22 +0100

    winningCombinations array added

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ const board = document.getElementById("board");
 const square = document.getElementsByClassName("square");
 const players = ["X", "O"];
 let currentPlayer = players[0];
+
 const endMessage = document.createElement('h4');
 endMessage.textContent = `X's turn`;
 endMessage.style.marginTop = '30px';
 endMessage.style.textAlign = 'center';
 board.after(endMessage);
+
+const winningCombinations =
+[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
