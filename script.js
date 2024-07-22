939366954b529f097aee235704fab979d26c84a0
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2024-07-22 12:25:20 +0100

    endMessage

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const board = document.getElementById("board");
 const square = document.getElementsByClassName("square");
 const players = ["X", "O"];
 let currentPlayer = players[0];
+const endMessage = document.createElement('h4');
+endMessage.textContent = `X's turn`;
+endMessage.style.marginTop = '30px';
+endMessage.style.textAlign = 'center';
+board.after(endMessage);
